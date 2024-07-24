9c66a4645f97501dd91f94750b61760573b2d670
jinho kim <jinhoo@naver.com>
2024-07-24 15:21:27 +0900

    commit after add file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def git_test(name):
     print("git setting & git first commit")
     print("git commit log modify --amend")
+    print("git commit file add & fix with .gitignore")
 
 if __name__ == '__main__':
     git_test('PyCharm')
