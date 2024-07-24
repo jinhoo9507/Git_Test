bfc5c5b990eff672982039a96d519f8d587bee19
jinho kim <jinhoo@naver.com>
2024-07-24 15:19:03 +0900

    modify commit log --amend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def git_test(name):
-    print("git setting & git first commit")  # Press Ctrl+F8 to toggle the breakpoint.
+    print("git setting & git first commit")
+    print("git commit log modify --amend")
 
 if __name__ == '__main__':
     git_test('PyCharm')
