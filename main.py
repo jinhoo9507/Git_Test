23eebe202f57c15000b6912b843d799ec8ace815
jinho kim <jinhoo@naver.com>
2024-07-25 10:52:27 +0900

    branch merge

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def git_test(name):
     print("git setting & git first commit")
     print("git commit log modify --amend")
     print("git commit file add & fix with .gitignore")
+    print("remote1_branch")
 
 if __name__ == '__main__':
     git_test('PyCharm')
